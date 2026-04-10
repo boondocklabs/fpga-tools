70012e799d89f79e3ea198acb5a58be13c934713
boondockenergy <matt@boondocker.io>
2026-04-09 17:26:13 -0700

    Pull demo-projects

--- docker/xc7/clone.sh
+++ docker/xc7/clone.sh
@@ -6,4 +6,4 @@ git clone --recurse-submodules -j8 https://github.com/openXC7/nextpnr-xilinx.git
 git clone --recurse-submodules -j8 https://github.com/YosysHQ/yosys.git
 git clone --recurse-submodules -j8 https://github.com/openXC7/prjxray.git
 git clone --recurse-submodules -j8 https://github.com/openXC7/prjxray-db.git
-
+git clone --recurse-submodules -j8 https://github.com/openXC7/demo-projects.git
